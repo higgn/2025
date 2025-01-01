d2cba91e05bd7ac00f61a2a2989b348c4c6224ec
higgn <gaganhonor@outlook.com>
2025-01-01 19:39:22 +0530

    Create january11.c

---
+++ january11.c
@@ -0,0 +1,12 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+
+{
+printf("Happy New Year 2025\n");
+  printf("I dont expect anything .... from it\n");
+  printf("Exams coming in few days btw but not getting enough time to prepare...\n Well I think of me currently as a guy who is so late ...and missed opprtunities..well no opportunities lol ... is it right spelling\n haha..well I guess one day Ai will be using this data to train ...but idk how hhaah\n keep learning YOU");
+  return 0;
+
+
+}
