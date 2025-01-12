1f2c8426c8a2f95789e56fa12068445a621c5c7d
higgn <gaganhonor@outlook.com>
2025-01-12 21:25:26 +0530

    Create JanuaryDD.pl

---
+++ JanuaryDD.pl
@@ -0,0 +1,8 @@
+sub motivation {
+    print "Hey you! 🚀\n";
+    print "Missing a few days doesn't define your journey.\n";
+    print "What matters is that you keep going.\n";
+    print "You're doing amazing, and you've got this! 💪\n";
+}
+
+motivation();
